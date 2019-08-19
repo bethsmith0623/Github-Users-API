7efe251ae3d329f63c0bfe8cf32371f3b9b3f6d3
Beth Smith <bethsmith0623@gamil.com>
2019-08-19 16:12:47 -0500

    renders user info

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,24 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+const rootURL = 'https://api.github.com/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {userData: null});
 });
 
 router.post('/', function(req, res, next) {
-  console.log(`username: ${req.body.username}`);
-  res.render('index');
+  var options = {
+    url: rootURL + 'users/' + req.body.username, 
+    headers: {
+      'User-Agent': 'bethsmith0623',
+      'Authorization': 'token ' + process.env.GITHUB_TOKEN 
+    }
+  };
+  request(options, function(err, response, body){
+      res.render('index', {userData: body});
+  });
 });
 
 
